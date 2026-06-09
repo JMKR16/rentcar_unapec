6f002ff8c1a9d1ff5dcba831b8a00296c0d41bbe
JoseMR16 Ortega <tu_correo@ejemplo.com>
2026-06-09 15:51:34 -0400

    Formulario de la tabla clientes

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from database import configurar_db
 
 app = Flask(__name__)
-
-# Conectar el puente de datos
 mysql = configurar_db(app)
+
 
 @app.route('/')
 def inicio():
@@ -12,9 +11,28 @@ def inicio():
     cursor.execute("SELECT * FROM clientes")
     lista_clientes = cursor.fetchall()
     cursor.close()
-    
-    # Aquí se le dice  a Flask que abra la plantilla y le inyecte los datos
     return render_template('clientes.html', clientes_pantalla=lista_clientes)
+
+
+@app.route('/guardar_cliente', methods=['POST'])
+def guardar_cliente():
+    nombre = request.form['txt_nombre']
+    cedula = request.form['txt_cedula']
+    tarjeta = request.form['txt_tarjeta']
+    limite = request.form['txt_limite']
+    tipo = request.form['sel_tipo']
+    
+    cursor = mysql.connection.cursor()
+    cursor.execute("""
+        INSERT INTO clientes (nombre, cedula, no_tarjeta_cr, limite_credito, tipo_persona)
+        VALUES (%s, %s, %s, %s, %s)
+    """, (nombre, cedula, tarjeta, limite, tipo))
+    
+    mysql.connection.commit()
+    cursor.close()
+    
+
+    return redirect(url_for('inicio'))
 
 if __name__ == '__main__':
     app.run(debug=True)
--- database.py
+++ database.py
@@ -1,22 +1,20 @@
 from flask_mysqldb import MySQL
 
 def configurar_db(app):
-    # 1. Dirección del servidor (MI pc)
+
     app.config['MYSQL_HOST'] = 'localhost'
     
-    # 2. Usuario administrador de MySQL
+   
     app.config['MYSQL_USER'] = 'root'
     
-    # 3. La contraseña del cliente de mysql de mi maquina
+    
     app.config['MYSQL_PASSWORD'] = 'Mysql10616'
     
-    # 4. El nombre exacto de la base de datos creada
+    
     app.config['MYSQL_DB'] = 'rentcar_unapec'
     
-    # 5. Formato de respuesta: Le pedimos a MySQL que nos devuelva los datos 
-    # en forma de "Diccionario" de Python (Clave: Valor), que es más fácil de leer.
+   
     app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
     
-    # Inicializa el conector
     mysql = MySQL(app)
     return mysql
